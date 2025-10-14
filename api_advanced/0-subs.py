31203a55c432537777575d2d4ed060a5796bc5f2
ISHIMWE Jonathan <j.ishimwe4@alustudent.com>
2025-10-14 15:31:34 +0200

    Update 0-subs.py

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """
-Script that queries the Reddit api and returns the number of subscribers for 
-a given subreddit.
+Module that queries the Reddit API and returns the number of subscribers
+for a given subreddit.
 """
 
 import requests
@@ -10,11 +10,11 @@ import requests
 def number_of_subscribers(subreddit):
     """
     Returns the number of subscribers for a given subreddit.
-    And if subreddit is invalid it returns 0.
+    If the subreddit is invalid, returns 0.
     """
     if not isinstance(subreddit, str) or subreddit == "":
         return 0
-    
+
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {'User-Agent': 'MyRedditBot/1.0 (by u/Jonathan)'}
 
@@ -22,7 +22,7 @@ def number_of_subscribers(subreddit):
         response = requests.get(url, headers=headers, allow_redirects=False)
         if response.status_code != 200:
             return 0
-        
+
         data = response.json().get("data")
         return data.get("subscribers", 0)
     except Exception:
