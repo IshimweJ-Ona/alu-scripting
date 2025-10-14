d7982891443e7659752ec04b5f1ae620fc14f44b
ISHIMWE Jonathan <j.ishimwe4@alustudent.com>
2025-10-14 15:30:32 +0200

    Update 0-subs.py

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -1,33 +1,29 @@
 #!/usr/bin/python3
 """
-A functioon that querries the reddit api and returns the number of subscribers (not active users, total subscribers)
-for a given subreddit. if invalid subreddit is given, it returns 0.
-requirements: def_number_subscribers(subreddit),
-             if not valid subreddit returns 0
-             invalid subreddit may return a redirect to search results.and folow the redirects.
+Script that queries the Reddit api and returns the number of subscribers for 
+a given subreddit.
 """
 
 import requests
-import sys
+
 
 def number_of_subscribers(subreddit):
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {
-        "User-Agent": "python:0-subs:v1.0 (by /u/Background_Panic9162)"
-    }
-    try:
-        response = requests.get(url, headers=headers, allow_redirects=True)
-        if response.status_code == 200:
-            data = response.json()
-            return data['data']['subscribers']
-        else:
-            return 0
-    except requests.RequestException:
+    """
+    Returns the number of subscribers for a given subreddit.
+    And if subreddit is invalid it returns 0.
+    """
+    if not isinstance(subreddit, str) or subreddit == "":
         return 0
+    
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {'User-Agent': 'MyRedditBot/1.0 (by u/Jonathan)'}
 
-if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: {} <subreddit>".format(sys.argv[0]))
-    else:
-        subreddit = sys.argv[1]
-        print(number_of_subscribers(subreddit))
+    try:
+        response = requests.get(url, headers=headers, allow_redirects=False)
+        if response.status_code != 200:
+            return 0
+        
+        data = response.json().get("data")
+        return data.get("subscribers", 0)
+    except Exception:
+        return 0
