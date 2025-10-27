9ee4d6cfd08527cf3286bcf0c6eea4e38441f889
IshimweJ-Ona <j.ishimwe4@alustudent.com>
2025-10-27 06:31:46 +0000

    Update hot list Reddit api

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,38 +1,34 @@
 #!/usr/bin/python3
-"""
-Module that queries the Reddit API and prints the titles
-of the first 10 hot posts for a given subreddit.
+""""
+Function that querries the Reddit API and prints the titles of the first 10 hot posts
+listed for a given subreddit.
 """
 
 import requests
 
-
 def top_ten(subreddit):
+    """Prints titles of 10 first hot posts for a given subreddit.
+    If not a valid subreddit will print None.
     """
-    Prints the titles of the first 10 hot posts for a given subreddit.
-    If the subreddit is invalid, returns None.
-    """
-    if not isinstance(subreddit, str) or subreddit == "":
-        return None
-
-    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
-    headers = {'User-Agent': 'MyRedditBot/1.0 (by u/Jonathan)'}
+    url = "https://www.reddit.com/r/{subreddit}/hot.json".format(subreddit)
+    headers = {'User-Agent': 'python:1-top_ten:v1.0 (by /u/Background_Panic9162)'}
     params = {'limit': 10}
 
+    response = requests.get(url, headers=headers, params=params, allow_redirects=False)
+
+    #Check invalid subreddit or API error
+    if response.status_code != 200:
+        print("None")
+        return
+    
     try:
-        response = requests.get(
-            url, headers=headers, params=params, allow_redirects=False
-        )
-
-        if response.status_code != 200:
-            return None
-
-        posts = response.json().get("data", {}).get("children", [])
-        if not posts:
-            return None
-
-        for post in posts:
+        results = response.json().get("data", {}).get("children", [])
+        if not results:
+            print("None")
+            return
+        
+        for post in results:
             print(post.get("data", {}).get("title"))
-
     except Exception:
-        return None
+        print("None")
+
