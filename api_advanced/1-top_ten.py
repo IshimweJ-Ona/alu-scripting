230c894fad2b317b8e8793ca3d476cc1853ba21e
ISHIMWE Jonathan <j.ishimwe4@alustudent.com>
2025-10-27 08:46:13 +0200

    Update 1-top_ten.py

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -2,38 +2,38 @@
 """
 Queries the Reddit API and prints the titles of the first 10 hot posts
 listed for a given subreddit.
-
-Prototype: def top_ten(subreddit)
-If not a valid subreddit, print None.
-NOTE: Invalid subreddits may return a redirect to search results.
-Ensure that you are not following redirects.
 """
 
 import requests
 import sys
 
+
 def top_ten(subreddit):
-    url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
-    headers = {
-        "User-Agent": "python:1-top_ten:v1.0 (by /u/Background_Panic9162)"
-    }
+    """Prints the titles of the first 10 hot posts of a subreddit."""
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    headers = {"User-Agent": "Mozilla/5.0"}
+    params = {"limit": 10}
 
     try:
-        response = requests.get(url, headers=headers, allow_redirects=False)
-        if response.status_code == 200:
-            data = response.json()
-            posts = data.get('data', {}).get('children', [])
-            if not posts:
-                print("None")
-            else:
-                for post in posts:
-                    print(post['data']['title'])
-        else:
+        response = requests.get(url, headers=headers,
+                                params=params, allow_redirects=False)
+        if response.status_code != 200:
             print("None")
+            return
+
+        posts = response.json().get("data", {}).get("children", [])
+        if not posts:
+            print("None")
+            return
+
+        for post in posts:
+            print(post.get("data", {}).get("title"))
+
     except requests.RequestException:
         print("None")
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     if len(sys.argv) < 2:
         print("Please pass an argument for the subreddit to search.")
     else:
